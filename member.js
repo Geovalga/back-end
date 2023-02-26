1f581c69d7e6bbd8a88b411905a47638eb8b79dc
Geovalga <113454599+Geovalga@users.noreply.github.com>
2023-02-26 11:42:18 +0800

    Update member.js

--- member.js
+++ member.js
@@ -1,9 +1,28 @@
 module.exports.data = [
     {
+        id: "T01",
+        nim: 0501710013,
+        nama: "Pollatu, Chrysto",
+        noTelp: "+6282189235320"
+    },
+    {
+        id: "AG01",
+        nim: 105022020001,
+        nama: "Rompis, Abraham Laurens William",
+        noTelp: "+6282348760604"
+    },
+    {
+        id: "AG02",
+        nim: 105022010050,
+        nama: "Rampen, Girard",
+        noTelp: "+6282393932271"
+    },
+    {
         id: "S11910124",
         nim: 105021910051,
         nama: "Geovalga Fransiscus Lim",
         noTelp: "0813444330533"
     },
+    
   
 ];
