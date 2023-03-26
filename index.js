80cbffa288a047d3230c3fbb41754de23ed86fa4
Geovalga.F.Lim <liemgeovalga@example.com>
2023-03-26 15:25:25 +0800

    Exercise#7

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path')
+const multer = require('multer');
 let users = require('./users');
-
 
 const app = express();
 const port = 3000;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+// CORS Handling
+app.use(cors({
+    origin: 'http://127.0.0.1:5500',
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+
+
+// Log using Morgan
+app.use(morgan('combined'), (req, res, next) => {
+    next();
+});
 
 // GET Default
 app.get('/', (req, res) => {
     res.send(`
-        <h1>Latihan Backend</h1>
+        <h1>Method Server GET,POST,PUT,DELETE</h1>
     `);
 });
 
@@ -37,8 +51,60 @@ app.get('/users/:name', (req, res) => {
     }
 });
 
+// PUT users name
+app.put('/users/:name', (req, res) => {
+    const {name} = req.body;
+    
+    if(users.filter(r => r.name.toLowerCase() === req.params.name.toLowerCase()).length === 0) {
+        res.status(400).send(JSON.stringify({
+            message: "Masukkan data yang akan diubah."
+        }));
+    }
+    else {
+        users.forEach(r => {
+            if(r.name.toLowerCase() === req.params.name.toLowerCase()) {
+                r.name = name;
+            }
+        });
 
-// Error handling
+        res.send(users);
+    }
+});
+
+// DELETE users name
+app.delete('/users/:name', (req, res) => {
+    if(users.filter(r => r.name.toLowerCase() === req.params.name.toLowerCase()).length === 0) {
+        res.send(JSON.stringify({
+            message: "Data user tidak ditemukan."
+        }));
+    }
+    else {
+        users = users.filter(r => r.name.toLowerCase() !== req.params.name.toLowerCase());
+        res.send(users);
+    }
+});
+
+// POST
+// Konfigurasi penyimpanan file menggunakan Multer
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, 'public/');
+    },
+    filename: function(req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname));
+    }
+  });
+  
+  // Konfigurasi upload file menggunakan Multer
+  const upload = multer({ storage: storage });
+  
+  // Handler untuk endpoint /upload
+  app.post('/upload', upload.single('image'), function(req, res, next) {
+    // Jika file berhasil diunggah, kirimkan respons
+    res.json({ message: 'File uploaded successfully.' });
+  });
+
+// penanganan error
 app.use((err, req, res, next) => {
     console.log(err);
     res.status(500).send(JSON.stringify({
@@ -54,7 +120,5 @@ app.use((req, res, next) => {
         message: "resource tidak ditemukan.",
     }));
 });
-
-
 
 app.listen(port, () => console.log(`Server is running at http://localhost:${port}.`));
